817c4c76d0c992454257c4b96c3915f92dbd6148
Marie-Louise Grognet <marielouise.grognet@gmail.com>
2025-08-28 10:52:25 +0200

    ajout de test pour les erreur 404 et 500

--- src/__tests__/ErrorPage.js
+++ src/__tests__/ErrorPage.js
@@ -22,4 +22,23 @@ describe('Given I am connected on app (as an Employee or an HR admin)', () => {
       expect(screen.getAllByText(error)).toBeTruthy()
     })
   })
+
+  describe('When ErrorPage is called with HTTP errors', () => {
+  test('Then, it should render ErrorPage with 404 error message', () => { // Ecriture d'un test pour erreur 404
+    const error404 = '404 Not Found'
+    const html = ErrorPage(error404)
+    document.body.innerHTML = html
+    expect(screen.getAllByText('Erreur')).toBeTruthy()
+    expect(screen.getByTestId('error-message').textContent.trim()).toBe(error404)
+  })
+
+  test('Then, it should render ErrorPage with 500 error message', () => { // Ecriture d'un test pour erreur 500
+    const error500 = '500 Internal Server Error'
+    const html = ErrorPage(error500)
+    document.body.innerHTML = html
+    expect(screen.getAllByText('Erreur')).toBeTruthy()
+    expect(screen.getByTestId('error-message').textContent.trim()).toBe(error500)
+  })
+})
+
 })
